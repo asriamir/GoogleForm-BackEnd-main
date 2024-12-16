6c2ad77756f231a4e0318b126ebaa0fba2a51da5
amir asri <147535490+asriamir@users.noreply.github.com>
2024-12-16 13:49:32 +0330

    Swagger and Redoc were added to the URLs.

--- GoogleForm/urls.py
+++ GoogleForm/urls.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Forms API",
+      default_version='v1',
+      description="Detailed documentation of the forms API, covering all endpoints and features.",
+      terms_of_service="https://www.example.com/terms/",
+      contact=openapi.Contact(email="contact@formsapi.local"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('forms.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-docs'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-docs'),
 
 ]
